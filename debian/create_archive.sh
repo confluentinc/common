45d609ff3bcfbfc63bf731e678b2ccecfe13178e
Xiang Li <lixiangwd@gmail.com>
2021-05-21 00:49:08 -0700

    Update create_archive.sh

--- debian/create_archive.sh
+++ debian/create_archive.sh
@@ -10,10 +10,6 @@ if [ -z ${PACKAGE_TITLE} -o -z ${VERSION} -o -z ${DESTDIR} ]; then
     exit 1
 fi
 
-if [[ -n ${SECURITY_PATCH} ]]; then
-VERSION=${VERSION}-${SECURITY_PATCH}
-fi
-
 BINPATH=${PREFIX}/bin
 LIBPATH=${PREFIX}/share/${PACKAGE_TITLE}
 DOCPATH=${PREFIX}/share/doc/${PACKAGE_TITLE}
